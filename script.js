55c8df9e14f8e9b7a237b5879ac52bc6cf9f4aff
Ogu <august.gros@gmail.com>
2022-04-25 17:09:08 +0200

    add accordeon

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-const boxTxt = document.querySelector(".boxTxt");
+/* const boxTxt = document.querySelector(".boxTxt");
+const testTxt = document.querySelector(".testTxt");
+const titleTxt = document.querySelector(".titleTxt")
 
+let counter = 0;
 boxTxt.addEventListener("click", function(){
+    counter++;
+    switch (counter % 2) {
+        case 0:
+            testTxt.classList.add("hidden");
+            titleTxt.classList.remove("font-bold");
+            break;
     
-})
+        default:
+            testTxt.classList.remove("hidden");
+            titleTxt.classList.add("font-bold");
+            break;
+    }
+ 
+}) */
+
+let acc = document.querySelectorAll(".accordion");
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let boxAcc = this.nextElementSibling;
+    if (boxAcc.style.maxHeight) {
+      boxAcc.style.maxHeight = null;
+    } else {
+      boxAcc.style.maxHeight = boxAcc.scrollHeight + "px";
+    } 
+  });
+}
