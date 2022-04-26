46134598bfa69944b5858f31d174418bdc63dea6
Ogu <august.gros@gmail.com>
2022-04-26 09:01:38 +0200

    fin

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-/* const boxTxt = document.querySelector(".boxTxt");
-const testTxt = document.querySelector(".testTxt");
-const titleTxt = document.querySelector(".titleTxt")
-
-let counter = 0;
-boxTxt.addEventListener("click", function(){
-    counter++;
-    switch (counter % 2) {
-        case 0:
-            testTxt.classList.add("hidden");
-            titleTxt.classList.remove("font-bold");
-            break;
-    
-        default:
-            testTxt.classList.remove("hidden");
-            titleTxt.classList.add("font-bold");
-            break;
-    }
- 
-}) */
+/* Version Fonctionnelle mais sans fermeture auto */
 
 let acc = document.querySelectorAll(".accordion");
 
@@ -32,3 +13,23 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+/* Version rire */
+
+/* let acc = document.querySelectorAll(".accordion");
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    let boxAcc = document.querySelectorAll(".boxAcc")
+    boxAcc.forEach(e => {
+      e.classList.remove("active");
+      this.classList.toggle("active");
+      if (e.style.maxHeight) {
+        e.style.maxHeight = null;
+      } else {
+        e.style.maxHeight = e.scrollHeight + "px";
+      }
+    }); 
+  });
+} */
